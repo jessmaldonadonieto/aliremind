728d8f8831769376aa006804719009c351fc9e8f
jessmaldonadonieto <jmaldonado.mad@gmail.com>
2026-09-04 11:36:24 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Pega aquí la URL de tu aplicación web de Google Apps Script.
+// No pongas claves privadas en este archivo.
+window.ALIREMIND_SEND_ENDPOINT = "https://script.google.com/macros/s/AKfycbySZDprosKCymKSpMmuacekgxHHYyarhd6Uf8rFNFukR491QPrw33AMazxkcEmdvaWe2A/exec";
