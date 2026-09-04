2dc0c6a71a7e38688b7a67d83955843661cfba68
jessmaldonadonieto <jmaldonado.mad@gmail.com>
2026-09-04 12:53:58 +0200

    Update ALIREMIND_SEND_ENDPOINT URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Pega aquí la URL de tu aplicación web de Google Apps Script.
 // No pongas claves privadas en este archivo.
-window.ALIREMIND_SEND_ENDPOINT = "https://script.google.com/macros/s/AKfycbySZDprosKCymKSpMmuacekgxHHYyarhd6Uf8rFNFukR491QPrw33AMazxkcEmdvaWe2A/exec";
+window.ALIREMIND_SEND_ENDPOINT = "https://script.google.com/macros/s/AKfycbyltrl8TA8-RGL_j4Yd6n47WI7Oq9tE2LygetrAjh2SpYv_VjXRysdO6vJVZNLanjcMVA/exec";
